8b6e79ab86cd6e632bc589ade6fe1488c44db408
rabobahago <rabobahago@gmail.com>
2020-10-25 06:11:56 +0100

    material ui core added

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import NewsCard from '../NewsCard/NewsCard'
+import { Grid, Grow, Typography } from '@material-ui/core'
 
 const NewsCards = ({ articles }) => {
   return (
-    <div>
-      {articles.map((articles, i) => (
-        <NewsCard />
-      ))}
-    </div>
+    <Grow in>
+      <Grid container alignItems="stretch" spacing={3}>
+        {articles.map((articles, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <NewsCard articles={articles} i={i} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   )
 }
 export default NewsCards
