3170a50068c9f1cc05a4c185140232fa759a836b
rabobahago <rabobahago@gmail.com>
2020-10-26 09:05:44 +0100

    more typography added to project

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,8 +1,48 @@
 import React from 'react'
-const NewsCard = () => {
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@material-ui/core'
+const NewsCard = ({
+  articles: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
   return (
     <div>
-      <h2>hello NewsCard</h2>
+      <Card>
+        <CardActionArea>
+          <CardMedia image={urlToImage} />
+          <div>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="h2"
+            ></Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="h2"
+            ></Typography>
+          </div>
+          <Typography gutterBottom variant="h5"></Typography>
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+            ></Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary"></Button>
+          <Typography variant="h5" color="textSecondary"></Typography>
+        </CardActions>
+      </Card>
     </div>
   )
 }
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import NewsCard from '../NewsCard/NewsCard'
 import { Grid, Grow, Typography } from '@material-ui/core'
+import useStyles from './styles'
 
 const NewsCards = ({ articles }) => {
+  const classes = useStyles()
   return (
     <Grow in>
-      <Grid container alignItems="stretch" spacing={3}>
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
         {articles.map((articles, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
             <NewsCard articles={articles} i={i} />
