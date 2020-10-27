27cbb0f7fbda213a1819953052f77b1ddc38569e
rabobahago <rabobahago@gmail.com>
2020-10-27 07:15:34 +0100

    complete styled News Card components

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -8,42 +8,48 @@ import {
   Button,
   Typography,
 } from '@material-ui/core'
+import useStyles from './styles'
 const NewsCard = ({
   articles: { description, publishedAt, source, title, url, urlToImage },
   i,
 }) => {
+  const classes = useStyles()
   return (
-    <div>
-      <Card>
-        <CardActionArea>
-          <CardMedia image={urlToImage} />
-          <div>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="h2"
-            ></Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="h2"
-            ></Typography>
-          </div>
-          <Typography gutterBottom variant="h5"></Typography>
-          <CardContent>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-            ></Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary"></Button>
-          <Typography variant="h5" color="textSecondary"></Typography>
-        </CardActions>
-      </Card>
-    </div>
+    <Card className={classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          className={classes.media}
+          image={
+            urlToImage ||
+            'www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'
+          }
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
   )
 }
 export default NewsCard
